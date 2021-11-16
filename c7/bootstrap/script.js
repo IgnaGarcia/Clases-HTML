8d1f45f80a256f6a33f68b04ec26ebec25f57e17
Igna Garcia <gnachoxp@gmail.com>
2021-11-16 12:01:15 -0300

    clase 16-11 js y maquetado a fondo

---
+++ c7/bootstrap/script.js
@@ -0,0 +1,35 @@
+function onClick(){
+    console.log("me clickearon")
+    toggleTheme()
+}
+
+function toggleTheme(){
+    if(isDark){
+        document.documentElement.style.setProperty('--fondo', '#e5e5e5');
+        document.documentElement.style.setProperty('--color', '#111');
+    }
+    else{
+        document.documentElement.style.setProperty('--fondo', '#252525');
+        document.documentElement.style.setProperty('--color', '#eee');
+    }
+    isDark = !isDark;
+}
+/*
+function unNumero(){
+    return 5;
+}
+
+function ejecutar(callback){
+    return callback()
+}
+
+console.log(ejecutar(onClick))
+*/
+function main(){
+    toggleTheme()
+
+    const btn = document.querySelector("#btn")
+    btn.addEventListener("click", onClick)
+}
+var isDark = true
+main()
